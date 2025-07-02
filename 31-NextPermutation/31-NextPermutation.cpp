79fa0a1cf7730ef0294aba7df6babc37d80cc219
Aniket Mishra <aniketmishrarr@gmail.com>
2025-07-02 17:43:34 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,9 @@
+// Last updated: 7/2/2025, 5:43:33 PM
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+        
+        
+    }
+};
