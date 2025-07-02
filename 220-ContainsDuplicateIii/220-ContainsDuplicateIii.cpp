1c4139788186fbe0720043d7bf627180ce0d7a94
Aniket Mishra <aniketmishrarr@gmail.com>
2025-07-02 17:43:06 +0530

    Create 220-ContainsDuplicateIii

---
+++ 220-ContainsDuplicateIii/220-ContainsDuplicateIii.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/2/2025, 5:43:05 PM
+class Solution {
+ public:
+  bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff,
+                                     int valueDiff) {
+    set<long> window;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      if (const auto it =
+              window.lower_bound(static_cast<long>(nums[i]) - valueDiff);
+          it != window.cend() && *it - nums[i] <= valueDiff)
+        return true;
+      window.insert(nums[i]);
+      if (i >= indexDiff)
+        window.erase(nums[i - indexDiff]);
+    }
+
+    return false;
+  }
+};
