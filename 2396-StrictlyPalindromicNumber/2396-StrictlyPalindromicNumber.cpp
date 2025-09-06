de229310a0ddf5e399da781acbfc9a3371946f74
Aniket Mishra <aniketmishrarr@gmail.com>
2025-09-06 14:11:46 +0530

    File updated at 9/6/2025, 2:11:45 PM

--- 2396-StrictlyPalindromicNumber/2396-StrictlyPalindromicNumber.cpp
+++ 2396-StrictlyPalindromicNumber/2396-StrictlyPalindromicNumber.cpp
@@ -1,7 +1,15 @@
-// Last updated: 9/6/2025, 2:07:20 PM
+// Last updated: 9/6/2025, 2:11:45 PM
 class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
-        return false;
+        // if (n==0){
+        //     return true;
+        // }
+        // if (n%2==0){
+        //     return false;
+        // }
+        // return true;
+         return false;
+        
     }
 };
