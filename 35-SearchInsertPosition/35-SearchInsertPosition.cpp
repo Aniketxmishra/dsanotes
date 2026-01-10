ab200ac7e3c26b78bf0e7fc2b59b7c7341b12159
Aniket Mishra <aniketmishrarr@gmail.com>
2026-01-10 14:54:06 +0530

    File updated at 1/10/2026, 2:54:05 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.cpp
+++ 35-SearchInsertPosition/35-SearchInsertPosition.cpp
@@ -1,23 +1,22 @@
-// Last updated: 7/2/2025, 5:43:28 PM
-class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low= 0;
-        int high = n-1;
-        int ans = n;
-
-        while(low<=high){
-            int mid = low+ (high-low)/2;
-
-            if(nums[mid]>=target){
-                ans=mid;
-                high = mid-1;
-            }
-            else low = mid +1;
-        }
-
-        return ans;
-        
-    }
-};
+// Last updated: 1/10/2026, 2:54:05 PM
+1class Solution {
+2public:
+3    int searchInsert(vector<int>& nums, int target) {
+4        int low=0;
+5        int high = nums.size()-1;
+6        while(low<=high){
+7            int mid = low + (high-low)/2;
+8            if(nums[mid]== target){
+9                return mid;
+10            }
+11            if(nums[mid]<target){
+12                low= mid+1;
+13            }
+14            else{
+15                high= mid-1;
+16            }
+17
+18        }
+19        return low;
+20    }
+21};
